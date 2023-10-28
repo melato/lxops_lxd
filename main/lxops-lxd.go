00a83ca2e31e8f7ed39a52cac59eba78704127df
Alex Athanasopoulos <alex@melato.org>
2023-10-29 00:31:09 +0300

    added comment about extra "template" command.

--- main/lxops-lxd.go
+++ main/lxops-lxd.go
@@ -21,6 +21,8 @@ func main() {
 	lxops.InitConfigTypes()
 	client := &lxdutil.LxdClient{}
 	cmd := lxops.RootCommand(client)
+	// The template command is not part of lxops and may be removed,
+	// because it uses data types that are specific to LXD and may diverge from Incus.
 	templateOps := &lxdutil.TemplateOps{Client: client}
 	cmd.Command("template").Flags(templateOps).RunFunc(templateOps.Apply)
 	cmd.Command("version").NoConfig().RunMethod(func() { fmt.Println(version) })
